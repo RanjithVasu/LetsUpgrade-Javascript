d7e16aa1fceddf0ae4aec6e2223cbe6a227bb69b
RanjithVasu <36071299+RanjithVasu@users.noreply.github.com>
2020-07-14 21:33:58 +0530

    Add files via upload

---
+++ Day1/intro.js
@@ -0,0 +1,12 @@
+// alert("Click me to check")
+console.time('Time taken');
+var a = {name: "prasad",age:"25",city:"vellore"}
+var b = {name: "raj",age:"24",city:"chennai"}
+var c = {name: "ash",age:"23",city:"erode"}
+
+console.log("********* Ans for question one *********")
+console.error("Something went wrong reload the page")
+console.warn("Hey this is warning message , Nothing wrong be cool")
+
+console.table({a ,b ,c})
+console.timeEnd('Time taken');
---
+++ Day1/var-let-const.js
@@ -0,0 +1,37 @@
+//////////var
+
+console.log("********* Ans for question two *********")
+var name = "prasad";
+
+var age = 23;
+
+var nothing = false;
+
+var a;
+var b = 10;
+
+var c = null;
+
+console.log(a)
+console.log(b)
+console.log(c)
+
+
+///////////////Let
+
+{
+    let name = "Ranjith";
+    console.log("using let ", name)
+}
+console.log(name)
+
+///////////const
+
+const fruits = ["mango","apple"]
+console.log(fruits)
+fruits.push('banana')
+console.log(fruits)
+
+
+/////////////var
+
